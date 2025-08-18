25e556463567e0fbe980c1661e34c90620261b5d
Janne Mäkinen <janne.makinen@systemverification.com>
2025-08-18 12:50:03 +0200

    fix playwright ci 8

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,12 +1,12 @@
 // frontend/playwright.config.ts
 
 export default {
-  testDir: './tests',
+  testDir: './',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [['html', { outputFolder: 'playwright-report' }]],
+  reporter: 'html',
   use: {
     baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:5173',
     trace: 'on-first-retry',
