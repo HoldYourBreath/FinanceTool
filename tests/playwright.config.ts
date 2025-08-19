1677aba918826957f0cf9007435cccd363e90f15
Janne Mäkinen <bacrguy@gmail.com>
2025-08-19 08:03:50 +0200

    fix playwright ci running path

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -2,7 +2,8 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: '.',
+  testMatch: /.*\.spec\.ts$/,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
